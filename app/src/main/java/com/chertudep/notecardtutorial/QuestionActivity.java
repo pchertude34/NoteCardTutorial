5db19aefe3cb4ba4c9de8623f28d06fdc24cebd0
Payton Chertude <chertudp@ortegonstate.edu>
2016-09-07 20:39:18 -0700

    stage 1 of notecard tutorial

--- app/src/main/java/com/chertudep/notecardtutorial/QuestionActivity.java
+++ app/src/main/java/com/chertudep/notecardtutorial/QuestionActivity.java
@@ -2,14 +2,62 @@ package com.chertudep.notecardtutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuestionActivity extends AppCompatActivity
 {
+    private Button mNextButton;
+    private Button mPrevButton;
+    private Button mAnswerButton;
+    private TextView mQuestion;
+
+    private int position = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mAnswerButton = (Button) findViewById(R.id.answer_button);
+        mQuestion = (TextView) findViewById(R.id.question_text);
+
+        mQuestion.setText(Questions.QuestionList[position]);
+
+        mNextButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                position++;
+                mQuestion.setText(Questions.QuestionList[position]);
+            }
+        });
+
+        mPrevButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                position--;
+                mQuestion.setText(Questions.QuestionList[position]);
+            }
+        });
+
+        mAnswerButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/chertudep/notecardtutorial/Questions.java
@@ -0,0 +1,23 @@
+package com.chertudep.notecardtutorial;
+
+/**
+ * Created by Payton on 9/7/2016.
+ */
+public class Questions
+{
+    public static String[] QuestionList = {
+            "What is an Activity?",
+            "What is an Intent?",
+            "Where should string values be stored?",
+            "How do you save the state of an Activity?",
+            "What is a Fragment?"
+    };
+
+    public static String[] AnswerList = {
+            "An Activity is what is used to hold the content on the screen",
+            "An intent is used to start a new activity",
+            "String values for your layout should be stored in the values/strings.xml file",
+            "Package the content you wish to save in a bundle. That bundle is recovered when the activity is loaded",
+            "A fragment is a way to modulate displays"
+    };
+}
